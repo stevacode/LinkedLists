af0f227de62bfa6dcb7d8b354713aee8ea1f7152
stevacode <stefandsavic@gmail.com>
2026-07-15 18:15:31 +0200

    Update LinkedLists implementation

--- LinkedLists.cpp
+++ LinkedLists.cpp
@@ -9,12 +9,42 @@ struct Node
 {
 	int data;
 	struct Node* next;
-};
+}*first=NULL;
+
+void create(int A[], int n)
+{
+	int i;
+	struct Node* t, *last;
+	first = new Node;
+	first->data = A[0];
+	first->next = NULL;
+	last = first;
+	for (i = 1; i < n; i++)
+	{
+		t = new Node;
+		t->data = A[i];
+		t->next = NULL;
+		last->next = t;
+		last = t;
+	}
+}
+
+void display(struct Node* p)
+{
+	while (p != NULL)
+	{
+		cout << p->data << " ";
+		p = p->next;
+	}
+}	
 
 int main()
 {
-	struct Node* p;
-	p = new Node;
+	int A[] = { 3,5,7,10,15 };
+	create(A, 5);
+	//struct Node* p;
+	//p = new Node;
+	display(first);
 
 	return 0;
 }
