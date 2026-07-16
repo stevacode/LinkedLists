7766f1833d92b84cd553b902930cd1a806273145
stevacode <stefandsavic@gmail.com>
2026-07-16 03:07:57 +0200

    Update CountAndSum.cpp

--- CountAndSum.cpp
+++ CountAndSum.cpp
@@ -45,6 +45,15 @@ int count(struct Node* p)
 	return l;
 }
 
+int recursiveCount(struct Node* p)
+{
+	if (p != NULL)
+		return recursiveCount(p->next) + 1;
+	else
+		return 0;
+}
+
+
 int sum(struct Node* p)
 {
 	int s = 0;
@@ -56,13 +65,24 @@ int sum(struct Node* p)
 	return s;
 }
 
+int recursiveSum(struct Node* p)
+{
+	if (p == NULL)
+		return 0;
+	else
+		return recursiveSum(p->next) + p->data;
+}
+
 int main()
 {
 	int A[] = { 3,5,7,10,15 };
 	create(A, 5);
 	display(first);
+	cout << endl;
 	cout << "Count: " << count(first) << endl;
 	cout << "Sum: " << sum(first) << endl;
+	cout << "Recursive Count: " << recursiveCount(first) << endl;
+	cout << "Recursive Sum: " << recursiveSum(first) << endl;
 
 	return 0;
 }
